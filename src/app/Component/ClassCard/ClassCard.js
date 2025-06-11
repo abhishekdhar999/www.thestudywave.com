3eec19693de8f7a94f11714bdac2db775bc22f44
Abhishek Dhar <dhar.abhishek999@gmail.com>
2025-06-11 16:13:15 +0530

    yes is a two

--- src/app/Component/ClassCard/ClassCard.js
+++ src/app/Component/ClassCard/ClassCard.js
@@ -144,12 +144,12 @@ style={{ fill: "#231e23" }}
 
     
     <div className="p-4 border-t mx-8 mt-2 flex justify-center md:gap-24 md:flex-row flex-col">
-      <Link href={classData.meetingLink}>
+      <a href={classData.meetingLink}>
     <div type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 cursor-pointer">Join Class</div>
-    </Link>
-    <Link href={classData.boardLink}>
+    </a>
+    <a href={classData.boardLink}>
     <div type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 cursor-pointer">Open Whiteboard</div>
-    </Link>
+    </a>
     </div>
     </div>
     </>
